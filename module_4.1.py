8500f484b705dd00882edff3f536262f5ca89045
Pavlo Vashchyshyn <pavlo.vashchyshyn@gmail.com>
2021-12-13 13:40:45 +0100

    Add module_4.1.py

--- module_4.1.py
+++ module_4.1.py
@@ -1,9 +1,11 @@
-my_bool = True
-print(my_bool)
-print(bool('True'))
-
-palindrom = True
-if palindrom:
+palindrom_list = ['kajak', 'madam', 'potop']
+p = False
+p = True
+if p:
     print("The word is palindrom")
 else:
     print("The word is not palindrom")
+
+word = palindrom_list
+print(palindrom_list)
+print(palindrom_list[::-1])
