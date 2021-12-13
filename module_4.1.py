8820faf87bf6cb70afab376acfcb3970dd9db7d2
Pavlo Vashchyshyn <pavlo.vashchyshyn@gmail.com>
2021-12-13 12:02:55 +0100

    module_4.1.py

--- module_4.1.py
+++ module_4.1.py
@@ -1,36 +1,9 @@
-# A recursive function that
-# check a str[s..e] is
-# palindrome or not.
-def is_PalRec(st, s, e) :
-	
-	# If there is only one character
-	if (s == e):
-		return True
+my_bool = True
+print(my_bool)
+print(bool('True'))
 
-	# If first and last
-	# characters do not match
-	if (st[s] != st[e]) :
-		return False
-
-	# If there are more than
-	# two characters, check if
-	# middle substring is also
-	# palindrome or not.
-	if (s < e + 1) :
-		return is_PalRec(st, s + 1, e - 1);
-
-	return True
-
-def is_Palindrome(st) :
-	n = len(st)
-	
-	if (n == 0) :
-		return True
-	
-	return is_PalRec(st, 0, n - 1);
-
-st = "geeg"
-if (is_Palindrome(st)) :
-	print ("Yes")
-else :
-	print ("No")
+palindrom = True
+if palindrom:
+    print("The word is palindrom")
+else:
+    print("The word is not palindrom")
