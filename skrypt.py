3acf1fe03b9d9669ecb59073160546d9f4d87489
Pavlo Vashchyshyn <pavlo.vashchyshyn@gmail.com>
2021-12-16 21:21:38 +0100

    skrypt.py

---
+++ skrypt.py
@@ -0,0 +1,12 @@
+print("Greetings from file!")
+
+def greeting_from_file():
+    print("Greetings from file!")
+
+greeting_from_file()
+
+if __name__ == "__main__":
+    pass
+    # here you have a place to write code that will be
+    # executed after python <path_to_this_script.py>
+
