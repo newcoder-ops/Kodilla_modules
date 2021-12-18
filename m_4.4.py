ecd25b8b05be6a61823ab9f9febbb09a91850fc4
Pavlo Vashchyshyn <pavlo.vashchyshyn@gmail.com>
2021-12-18 23:49:59 +0100

    Add

--- m_4.4.py
+++ m_4.4.py
@@ -10,6 +10,19 @@ if __name__ == "__main__":
     age = int(sys.argv[1])
     print_maturity(age)
 
-
 print("The program was called with this parameters %s" % sys.argv[1:])
 print("The first parameter is %s" % sys.argv[1])
+
+text = {"John, Livia, Mark, Stephen"}
+
+with open("names.txt", 'r') as read_file:
+    for line in read_file.read().splitlines():
+        print(line)
+
+new_name = "Luke"
+with open("names.txt", 'a') as write_file:
+    write_file.write(new_name)
+
+new_name = "Luke"
+with open("new_names.txt", 'w') as write_file:
+    write_file.write(new_name)
