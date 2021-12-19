20c3cb2357fb7a58583304b6b5f856f0b465c141
Pavlo Vashchyshyn <pavlo.vashchyshyn@gmail.com>
2021-12-19 14:38:52 +0100

    Add

---
+++ import logging.py
@@ -0,0 +1,3 @@
+import logging
+logging.warn("warning!")
+logging.debug("debug")
--- m_4.4.py
+++ m_4.4.py
@@ -6,12 +6,12 @@ def print_maturity(age):
     else:
         print("You are a kiddo!")
 
-if __name__ == "__main__":
-    age = int(sys.argv[1])
-    print_maturity(age)
+#if __name__ == "__main__":
+    #age = int(sys.argv[1])
+    #print_maturity(age)
 
 print("The program was called with this parameters %s" % sys.argv[1:])
-print("The first parameter is %s" % sys.argv[1])
+#print("The first parameter is %s" % sys.argv[1])
 
 text = {"John, Livia, Mark, Stephen"}
 
@@ -27,6 +27,28 @@ new_name = "Luke"
 with open("new_names.txt", 'w') as write_file:
     write_file.write(new_name)
 
+# Moduł logging
 import logging
 logging.warn("warning!")
 logging.debug("debug")
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.warn("warning!")
+logging.debug("debug")
+
+import sys
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def print_maturity(age):
+    if age >= 18:
+        logging.info("You are an adult")
+    else:
+        logging.info("You are a kiddo!")
+
+if __name__ == "__main__":
+    logging.debug("The program was called with this parameters %s" % sys.argv[1:])
+    logging.debug("First parameter is %s" % sys.argv[1])
+    age = int(sys.argv[1])
+    print_maturity(age)
