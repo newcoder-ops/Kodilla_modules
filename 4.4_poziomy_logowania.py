bb38a8a5c012eb310f19fe8a259ef5f7f0eb3d7c
Pavlo Vashchyshyn <pavlo.vashchyshyn@gmail.com>
2021-12-20 13:40:06 +0100

    Add

--- 4.4_poziomy_logowania.py
+++ 4.4_poziomy_logowania.py
@@ -13,3 +13,8 @@ if __name__ == "__main__":
     logging.debug("First parameter is %s" % sys.argv[1])
     age = int(sys.argv[1])
     print_maturity(age)
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+# 2020-03-14 17:39:45,488 The program was called with this parameters ['1']
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile.log")
--- calculator.py
+++ calculator.py
@@ -1,1 +1,18 @@
 print("Welcome to calculator")
+import logging
+def calculate(choice):
+    if choice == "1":
+        logging.info("Dodajemy")
+        a = input("Podaj pierwszą liczbę")
+        b = input("Podaj drugą liczbę")
+        return int(a) + int(b)
+    if choice == "2":
+        logging.info("Obejmójemy")
+        a = input("Podaj pierwszą liczbę")
+        b = input("Podaj drugą liczbę")
+        return int(a) - int(b)
+    if choice == "3":
+choice = input("Wybierz 1 - dodawanie 2 - odejmowanie")
+result = calculate(choice)
+
+print(result)
