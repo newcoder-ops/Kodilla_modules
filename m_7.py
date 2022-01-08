dcfb34249ecc1aa224efa45051f1578ecdea9eaf
Pavlo Vashchyshyn <pavlo.vashchyshyn@gmail.com>
2022-01-08 18:23:23 +0100

    Add

--- m_7.py
+++ m_7.py
@@ -1,2 +1,15 @@
 class Car:
     pass
+my_car = Car()
+type(my_car)
+#__init__()
+
+class Car:
+   def __init__(self, make, model_name, top_speed, color):
+       self.make = make
+       self.model_name = model_name
+       self.top_speed = top_speed
+       self.color = color
+
+mustang = Car(make="Ford", model_name="Mustang", color="Red", top_speed=250)
+print(mustang.make)
