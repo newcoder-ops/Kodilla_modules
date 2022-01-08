d5b8418d2c9f2131a82a45d729607d2b470b3941
Pavlo Vashchyshyn <pavlo.vashchyshyn@gmail.com>
2022-01-08 23:54:02 +0100

    Add

--- m_7.py
+++ m_7.py
@@ -20,3 +20,13 @@ print(mustang)
 def __str__(self):
     return f'{self.color} {self.make} {self.model_name}'
 print(mustang)
+
+def __repr__(self):
+    return f"Car(make={self.make} model={self.model_name}, top_speed={self.top_speed}, color={self.color})"
+car = Car(make="Ford", model_name="Mustang", top_speed=250, color="Red")
+car
+print(car)
+
+car_one = Car(make="Ford", model_name="Mustang", top_speed=250, color="Red")
+car_two = Car(make="Ford", model_name="Mustang", top_speed=250, color="Red")
+car_one == car_two
